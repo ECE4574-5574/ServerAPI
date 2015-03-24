b82574d0ce9171294128cb89c634715662d87c45
ba2669 <ba2669@vt.edu>
2015-03-23 20:17:32 -0400

    Fixed broken build

--- HomeAutomationServer/App_Start/WebApiConfig.cs
+++ HomeAutomationServer/App_Start/WebApiConfig.cs
@@ -1,7 +1,9 @@
-﻿using System;
+﻿using HomeAPI.Controllers;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Web.Http.Dispatcher;
 
 namespace HomeAutomationServer
 {
