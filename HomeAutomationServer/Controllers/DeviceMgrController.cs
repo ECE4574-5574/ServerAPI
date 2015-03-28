746fad73eb177f4475caa6ce623e69e3594bcbab
ba2669 <ba2669@vt.edu>
2015-03-28 12:32:05 +0000

    Initial commit for Device Manager

---
+++ HomeAutomationServer/Controllers/DeviceMgrController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HomeAutomationServer.Controllers
+{
+    public class DeviceMgrController : ApiController
+    {
+
+        public object GetDeviceState()
+        {
+            return null;
+        }
+
+    }
+}
