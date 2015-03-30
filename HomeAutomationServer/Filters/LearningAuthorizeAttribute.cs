db8bb7bb4904361509a652c86612b1927bb99dbd
willrd9 <wr.doan@hotmail.com>
2015-03-29 22:58:46 -0400

    Added Authentication

---
+++ HomeAutomationServer/Filters/LearningAuthorizeAttribute.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web;
+using System.Web.Http.Filters;
+using System.Net.Http;
+using System.Threading;
+using System.Security.Principal;
+using System.Text;
+using HomeAutomationServer.Services;
+
+namespace HomeAutomationServer.Filters
+{
+    public class LearningAuthorizeAttribute : AuthorizationFilterAttribute
+    {
+
+        [Inject]
+        public UserRepository TheRepository { get; set; }
+
+        public override void OnAuthorization(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            //Case that user is authenticated using forms authentication 
+            //so no need to check header for basic authentication.
+            if (Thread.CurrentPrincipal.Identity.IsAuthenticated)
+            {
+                return;
+            }
+
+            var authHeader = actionContext.Request.Headers.Authorization;
+
+            if (authHeader != null)
+            {
+                if (authHeader.Scheme.Equals("basic", StringComparison.OrdinalIgnoreCase) &&
+                    !String.IsNullOrWhiteSpace(authHeader.Parameter))
+                {
+                    var credArray = GetCredentials(authHeader);
+                    var userName = credArray[0];
+                    var password = credArray[1];
+
+                    if (IsResourceOwner(userName, actionContext))
+                    {
+                        //You can use Websecurity or asp.net memebrship provider to login, for
+                        //for he sake of keeping example simple, we used out own login functionality
+                        if (TheRepository.GetUser(userName, password) != null)
+                        {
+                            var currentPrincipal = new GenericPrincipal(new GenericIdentity(userName), null);
+                            Thread.CurrentPrincipal = currentPrincipal;
+                            return;
+                        }
+                    }
+                }
+            }
+
+            HandleUnauthorizedRequest(actionContext);
+        }
+
+        private string[] GetCredentials(System.Net.Http.Headers.AuthenticationHeaderValue authHeader)
+        {
+
+            //Base 64 encoded string
+            var rawCred = authHeader.Parameter;
+            var encoding = Encoding.GetEncoding("iso-8859-1");
+            var cred = encoding.GetString(Convert.FromBase64String(rawCred));
+
+            var credArray = cred.Split(':');
+
+            return credArray;
+        }
+
+        private bool IsResourceOwner(string userName, System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            WebRequest request = WebRequest.Create("http://54.152.190.217:8080/UI/" + userName);
+            request.Method = "GET";
+            /*
+            var routeData = actionContext.Request.GetRouteData();
+            var resourceUserName = routeData.Values["userName"] as string;
+
+            if (resourceUserName == userName)
+            {
+                return true;
+            }
+            return false;
+             */
+        }
+
+        private void HandleUnauthorizedRequest(System.Web.Http.Controllers.HttpActionContext actionContext)
+        {
+            actionContext.Response = actionContext.Request.CreateResponse(HttpStatusCode.Unauthorized);
+
+            actionContext.Response.Headers.Add("WWW-Authenticate",
+                                               "Basic Scheme='eLearning' location='http://localhost:8323/account/login'");
+
+        }
+    }
+}
