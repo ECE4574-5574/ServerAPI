55656cb0715347654a5347e981921bf5720ca306
ba2669 <ba2669@vt.edu>
2015-03-30 14:41:07 -0400

    Added Sim Controller

---
+++ HomeAutomationServer/Controllers/SimController.cs
@@ -0,0 +1,24 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace HomeAutomationServer.Controllers
+{
+    [RoutePrefix("api/sim")]
+    public class SimController : ApiController
+    {
+
+        [Route("configuration/{id}")]
+        public object GetConfiguration(int id)
+        {
+            JObject obj = new JObject();
+            obj.Add("Id", id);
+            return obj;
+        }
+
+    }
+}
