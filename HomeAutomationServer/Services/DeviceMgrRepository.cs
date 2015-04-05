9b2038d56a5ce91c50398ea3e36e9de01db3caa8
enghokie <enghokie@vt.edu>
2015-04-05 16:13:21 -0400

    added device manager repo

---
+++ HomeAutomationServer/Services/DeviceMgrRepository.cs
@@ -0,0 +1,18 @@
+﻿using HomeAutomationServer.Services;
+using HomeAutomationServer.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Web;
+
+namespace HomeAutomationServer.Services
+{
+    public class DeviceMgrRepository
+    {
+        
+    }
+}
