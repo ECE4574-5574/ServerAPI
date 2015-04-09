12637b99e981670b1320302b15180c56b60f4354
nathodius <njh2986@vt.edu>
2015-04-08 21:45:12 -0400

    Remove Deployment Files
    This essentially reverts the last commit

--- before-install.bat
+++
@@ -1,3 +0,0 @@
-REM Install Internet Information Server (IIS).
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Import-Module -Name ServerManager
-c:\Windows\Sysnative\WindowsPowerShell\v1.0\powershell.exe -Command Install-WindowsFeature Web-Server
