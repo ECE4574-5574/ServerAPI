a28fe9a6fdc773ac13cdeccdc6c7629d73147abb
enghokie <enghokie@vt.edu>
2015-04-09 06:46:43 -0400

    commit

---
+++ api/ControlTypes.cs
@@ -0,0 +1,86 @@
+﻿/**
+ * Classes which represent meaningful quantities which can be measured in the system
+ */
+using System;
+
+namespace api
+{
+
+/**
+ * Base class which is used for contraining Devices, representing some quantity which can be
+ * measured/controlled.
+ */
+public class ControlTypes
+{
+	public ControlTypes()
+	{
+	}
+}
+
+/**
+ *  Light level in a room, as measured by brightness. Currently, unit-less.
+ */
+public class Light : ControlTypes
+{
+	public double Brightness
+	{
+		get;
+		set;
+	}
+}
+
+/**
+ * Temperature in a room, as measured in Celsius.
+ */
+public class Temperature : ControlTypes
+{
+	/**
+	 * Generic accessor, which returns underlying measure without explicit type.
+	 */
+	public double Temp
+	{
+		get
+		{
+			return _temp_c;
+		}
+		set
+		{
+			_temp_c = value;
+		}
+	}
+
+	/**
+	 * Property which always returns in Celsius
+	 */
+	public double C
+	{
+		get
+		{
+			return _temp_c;
+		}
+		set
+		{
+			_temp_c = value;
+		}
+	}
+
+	/**
+	 * Property which always returns in Farenheit
+	 */
+	public double F
+	{
+		get
+		{
+			return _temp_c * 1.8 + 32.0;
+		}
+		set
+		{
+			_temp_c = (value - 32.0) * (5.0 / 9.0);
+		}
+	}
+
+	protected double _temp_c;
+}
+
+}
+
---
+++ api/DeviceAPI.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Net.Http;
+using System.Collections.Generic;
+using api;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace api
+{
+
+public class Interfaces
+{
+	protected HttpClient _http;
+	protected Uri _server;
+
+	public Interfaces(Uri serverAddress)
+	{
+		_http = new HttpClient();
+		_server = serverAddress;
+	}
+
+	Device createDevice(Uri address, string name, string type, UInt64 house_id, UInt64 room_id = 0)
+	{
+		//TODO: Post to Server API to request the device be recorded, and get the device.
+		var device = (Device)Activator.CreateInstance(Type.GetType(type));
+		return device;
+	}
+
+	/**
+	 * Function to get a list of devices from the server, given parameters
+	 */
+	List<Device> getDevices(UInt64 houseID)
+	{
+		var devices = new List<Device>();
+		//TODO: Query all devices in a given house.
+		return devices;
+	}
+
+	List<Device> getDevices(UInt64 houseID, UInt64 roomID)
+	{
+		var devices = new List<Device>();
+		//TODO: Query all devices in a given room.
+		return devices;
+	}
+}
+}
+
