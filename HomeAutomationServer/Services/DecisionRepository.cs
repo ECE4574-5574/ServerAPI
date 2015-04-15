eb4528f87bd406a296958ddadada9a5faf0b76db
enghokie <enghokie@users.noreply.github.com>
2015-04-15 19:23:52 -0400

    Updated Exception Catching

--- HomeAutomationServer/Services/DecisionRepository.cs
+++ HomeAutomationServer/Services/DecisionRepository.cs
@@ -19,70 +19,79 @@ namespace HomeAutomationServer.Services
 
         public bool StateUpdate(JObject model)
         {
-
-            WebRequest request = WebRequest.Create(houseApiHost + "/" + (UInt64)model["HouseID"] + "/" + (UInt64)model["RoomID"] + "/" + (UInt64)model["DeviceID"]);
-            request.Method = "POST";
-
-            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-            {
-                streamWriter.Write(model.ToString());
-                streamWriter.Close();
-            }
-
             try
             {
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                WebRequest request = WebRequest.Create(houseApiHost + "/" + (UInt64)model["HouseID"] + "/" + (UInt64)model["RoomID"] + "/" + (UInt64)model["DeviceID"]);
+                request.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
                 {
-                    if (response.StatusCode != HttpStatusCode.OK)
-                        throw new Exception(String.Format(
-                           "Server error (HTTP {0}: {1}).",
-                           response.StatusCode,
-                           response.StatusDescription));
+                    streamWriter.Write(model.ToString());
+                    streamWriter.Close();
                 }
-            }
 
-            catch(Exception ex)
-            {
-                File.AppendAllText("HomeAutomationServer/logfile.txt", "House POST device request: " + ex.Message);
-                return false;
-            }
-
-            request = WebRequest.Create(storageUrl + "UD/" + (UInt64)model["HouseID"] + "/" + (UInt64)model["RoomID"] + "/" + (UInt64)model["DeviceID"]);
-            request.Method = "POST";
-
-            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-            {
-                streamWriter.Write(model.ToString());
-                streamWriter.Close();
-            }
-
-            try
-            {
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                try
                 {
-                    if (response.StatusCode != HttpStatusCode.OK)
-                        throw new Exception(String.Format(
-                           "Server error (HTTP {0}: {1}).",
-                           response.StatusCode,
-                           response.StatusDescription));
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        if (response.StatusCode != HttpStatusCode.OK)
+                            throw new Exception(String.Format(
+                               "Server error (HTTP {0}: {1}).",
+                               response.StatusCode,
+                               response.StatusDescription));
+                    }
                 }
+
+                catch (WebException ex)
+                {
+                    File.AppendAllText("HomeAutomationServer/logfile.txt", "House POST device request: " + ex.Message);
+                    return false;
+                }
+
+                request = WebRequest.Create(storageUrl + "UD/" + (UInt64)model["HouseID"] + "/" + (UInt64)model["RoomID"] + "/" + (UInt64)model["DeviceID"]);
+                request.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamWriter.Write(model.ToString());
+                    streamWriter.Close();
+                }
+
+                try
+                {
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        if (response.StatusCode != HttpStatusCode.OK)
+                            throw new Exception(String.Format(
+                               "Server error (HTTP {0}: {1}).",
+                               response.StatusCode,
+                               response.StatusDescription));
+                    }
+                }
+
+                catch (WebException ex)
+                {
+                    File.AppendAllText("HomeAutomationServer/logfile.txt", "Storage POST device request: " + ex.Message);
+                    return false;
+                }
+
+                try
+                {
+                    if (!appCache.AddDeviceBlob(model))
+                        throw new Exception("AppCache add device failed when adding: " + model.ToString());
+                }
+
+                catch (Exception ex)
+                {
+                    File.AppendAllText("HomeAutomationServer/logfile.txt", ex.Message);
+                    return false;
+                }
+
             }
 
-            catch (Exception ex)
+            catch(SystemException ex)
             {
-                File.AppendAllText("HomeAutomationServer/logfile.txt", "Storage POST device request: " + ex.Message);
-                return false;
-            }
-
-            try
-            {
-                if (!appCache.AddDeviceBlob(model))
-                    throw new Exception("AppCache add device failed when adding: " + model.ToString());
-            }
-
-            catch(Exception ex)
-            {
-                File.AppendAllText("HomeAutomationServer/logfile.txt", ex.Message);
+                File.AppendAllText("HomeAutomationServer/logfile.txt", "Could not parse the JSON data with the appropriate keys: " + ex.Message);
                 return false;
             }
 
@@ -93,30 +102,40 @@ namespace HomeAutomationServer.Services
         {
             JObject device = new JObject();
 
-            WebRequest request = WebRequest.Create(houseApiHost + (UInt64)model["HouseID"] + (UInt64)model["RoomID"] + (UInt64)model["DeviceID"]);
-            request.Method = "GET";
-
             try
             {
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-                {
-                    if (response.StatusCode != HttpStatusCode.OK)
-                        throw new Exception(String.Format(
-                           "Server error (HTTP {0}: {1}).",
-                           response.StatusCode,
-                           response.StatusDescription));
-                    var stream = response.GetResponseStream();
-                    var reader = new StreamReader(stream);
+                WebRequest request = WebRequest.Create(houseApiHost + (UInt64)model["HouseID"] + (UInt64)model["RoomID"] + (UInt64)model["DeviceID"]);
+                request.Method = "GET";
 
-                    string deviceString = reader.ReadToEnd();
-                    device = JObject.Parse(deviceString);
-                    return (bool)device["Enabled"];
+
+                try
+                {
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        if (response.StatusCode != HttpStatusCode.OK)
+                            throw new Exception(String.Format(
+                               "Server error (HTTP {0}: {1}).",
+                               response.StatusCode,
+                               response.StatusDescription));
+                        var stream = response.GetResponseStream();
+                        var reader = new StreamReader(stream);
+
+                        string deviceString = reader.ReadToEnd();
+                        device = JObject.Parse(deviceString);
+                        return (bool)device["Enabled"];
+                    }
+                }
+
+                catch (WebException ex)
+                {
+                    File.AppendAllText("HomeAutomationServer/logfile.txt", "House GET device request: " + ex.Message);
+                    return (bool)model["Enabled"];
                 }
             }
 
-            catch (Exception ex)
+            catch (SystemException ex)
             {
-                File.AppendAllText("HomeAutomationServer/logfile.txt", "House GET device request: " + ex.Message);
+                File.AppendAllText("HomeAutomationServer/logfile.txt", "Could not parse the JSON data with the appropriate keys: " + ex.Message);
                 return (bool)model["Enabled"];
             }
         }
