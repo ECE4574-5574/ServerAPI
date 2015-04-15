8f42c4a568e51c960e342bde28506b86c2682452
enghokie <enghokie@users.noreply.github.com>
2015-04-15 01:42:44 -0400

    Update DecisionRepository.cs

--- HomeAutomationServer/Services/DecisionRepository.cs
+++ HomeAutomationServer/Services/DecisionRepository.cs
@@ -7,68 +7,106 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
-using api;
 
 namespace HomeAutomationServer.Services
 {
     public class DecisionRepository
     {
-        private string houseApiHost = "";
-        private string appApiHost = "";
-        static Uri serverUri;
-        private Interfaces deviceInterface = new Interfaces(serverUri);
+        private string houseApiHost = "http://house_address:house_port/device/";
+        private string storageUrl = "";
 
-        public bool StateUpdate(UInt64 deviceid, bool state)
+        public bool StateUpdate(JObject model)
         {
-            string deviceString;
 
-            WebRequest request = WebRequest.Create(houseApiHost);
-            request.Method = "GET";
+            WebRequest request = WebRequest.Create(houseApiHost + "/" + (UInt64)model["HouseID"] + "/" + (UInt64)model["RoomID"] + "/" + (UInt64)model["DeviceID"]);
+            request.Method = "POST";
 
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
             {
-                if (response.StatusCode != HttpStatusCode.OK)
-                    throw new Exception(String.Format(
-                       "Server error (HTTP {0}: {1}).",
-                       response.StatusCode,
-                       response.StatusDescription));
-                var stream = response.GetResponseStream();
-                var reader = new StreamReader(stream);
-
-                deviceString = reader.ReadToEnd();
+                streamWriter.Write(model.ToString());
+                streamWriter.Close();
             }
 
-            Device myDevice = Interfaces.DeserializeDevice(deviceString, null, null);
-            //myDevice.Enable = state;
-            JObject device = JObject.Parse(myDevice.ToString());
-            //send to APP cache
-            // send to Storage
+            try
+            {
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception(String.Format(
+                           "Server error (HTTP {0}: {1}).",
+                           response.StatusCode,
+                           response.StatusDescription));
+                }
+            }
+
+            catch(Exception ex)
+            {
+                File.AppendAllText("HomeAutomationServer/logfile.txt", "House POST device request: " + ex.Message);
+                return false;
+            }
+
+            request = WebRequest.Create(storageUrl + "UD/" + (UInt64)model["HouseID"] + "/" + (UInt64)model["RoomID"] + "/" + (UInt64)model["DeviceID"]);
+            request.Method = "POST";
+
+            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+            {
+                streamWriter.Write(model.ToString());
+                streamWriter.Close();
+            }
+
+            try
+            {
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception(String.Format(
+                           "Server error (HTTP {0}: {1}).",
+                           response.StatusCode,
+                           response.StatusDescription));
+                }
+            }
+
+            catch (Exception ex)
+            {
+                File.AppendAllText("HomeAutomationServer/logfile.txt", "Storage POST device request: " + ex.Message);
+                return false;
+            }
+
+            // put in cache
 
             return true;
         }
 
-        public bool GetState(UInt64 deviceid)
+        public bool GetState(JObject model)
         {
             JObject device = new JObject();
 
-            WebRequest request = WebRequest.Create(houseApiHost);
+            WebRequest request = WebRequest.Create(houseApiHost + (UInt64)model["HouseID"] + (UInt64)model["RoomID"] + (UInt64)model["DeviceID"]);
             request.Method = "GET";
 
-             using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-             {
-                 if (response.StatusCode != HttpStatusCode.OK)
-                     throw new Exception(String.Format(
-                        "Server error (HTTP {0}: {1}).",
-                        response.StatusCode,
-                        response.StatusDescription));
-                 var stream = response.GetResponseStream();
-                 var reader = new StreamReader(stream);
+            try
+            {
+                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                {
+                    if (response.StatusCode != HttpStatusCode.OK)
+                        throw new Exception(String.Format(
+                           "Server error (HTTP {0}: {1}).",
+                           response.StatusCode,
+                           response.StatusDescription));
+                    var stream = response.GetResponseStream();
+                    var reader = new StreamReader(stream);
 
-                 string deviceString = reader.ReadToEnd();
-                 device = JObject.Parse(deviceString);
+                    string deviceString = reader.ReadToEnd();
+                    device = JObject.Parse(deviceString);
+                    return (bool)device["Enabled"];
+                }
             }
 
-            return (bool)device["Enabled"];
+            catch (Exception ex)
+            {
+                File.AppendAllText("HomeAutomationServer/logfile.txt", "House GET device request: " + ex.Message);
+                return (bool)model["Enabled"];
+            }
         }
     }
 }
