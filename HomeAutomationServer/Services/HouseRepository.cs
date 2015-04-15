e22528f8219b7f5793cb6d6d605a5d908b02893a
nathodius <njh2986@vt.edu>
2015-04-14 22:14:05 -0400

    House Posts Dev State Change
    Posts the updated device info (blob) to persistent storage and updates
    the state change cache*
    * The app won't receive the update until it makes a GET request

--- HomeAutomationServer/Services/HouseRepository.cs
+++ HomeAutomationServer/Services/HouseRepository.cs
@@ -13,29 +13,74 @@ using System.Web;
 
 namespace HomeAutomationServer.Services
 {
-    public class HouseRepository
-    {
-        public JObject GetHouse(string id)
-        {
-             WebRequest request = WebRequest.Create("http://54.152.190.217:8081/HI/" + id);
-            request.Method = "GET";
+	public class HouseRepository
+	{
+		public JObject GetHouse(string id)
+		{
+			WebRequest request = WebRequest.Create("http://54.152.190.217:8081/HI/" + id);
+			request.Method = "GET";
 
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
-            {
-                if (response.StatusCode != HttpStatusCode.OK)
-                    throw new Exception(String.Format(
-                    "Server error (HTTP {0}: {1}).",
-                    response.StatusCode,
-                    response.StatusDescription));
-                var stream = response.GetResponseStream();
-                var reader = new StreamReader(stream);
+			using(HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+			{
+				if(response.StatusCode != HttpStatusCode.OK)
+					throw new Exception(String.Format(
+						"Server error (HTTP {0}: {1}).",
+						response.StatusCode,
+						response.StatusDescription));
+				var stream = response.GetResponseStream();
+				var reader = new StreamReader(stream);
 
-                string houseString = reader.ReadToEnd();
-                return JObject.Parse(houseString);
-            }
-        }
+				string houseString = reader.ReadToEnd();
+				return JObject.Parse(houseString);
+			}
+		}
 
-        /*public JObject SaveHouse(string houseId, JToken model)
+		public bool postState(JObject deviceBlob)
+		{
+
+			//POST UD/HOUSEID/ROOMID/DEVICEID
+
+			string houseID;
+			string roomID;
+			string deviceID;
+
+			houseID = deviceBlob.GetValue("HOUSEID").ToString();
+			roomID = deviceBlob.GetValue("ROOMID").ToString();
+			deviceID = deviceBlob.GetValue("DEVICEID").ToString();
+
+			WebRequest request = WebRequest.Create(DeviceRepository.storageURL + "UD/" + houseID + "/" + roomID + "/" + deviceID);
+			request.ContentType = "application/json";
+			request.Method = "POST";
+
+			using(var streamWriter = new StreamWriter(request.GetRequestStream()))
+			{
+				streamWriter.Write(deviceBlob.ToString());
+				streamWriter.Flush();
+				streamWriter.Close();
+			}
+
+			try
+			{
+				using(HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+				{
+					if(response.StatusCode != HttpStatusCode.OK)
+						throw new Exception(String.Format(
+							"Server error (HTTP {0}: {1}).",
+							response.StatusCode,
+							response.StatusDescription));
+				}
+			}
+			catch(WebException we)
+			{
+				// always catches this exception even when the Jtoken is sent properly. 
+				// Gets an error saying Connection was closed.
+				//return false;
+			}
+
+			return true;
+		}
+
+		/*public JObject SaveHouse(string houseId, JToken model)
         {
             WebRequest request = WebRequest.Create("http://54.152.190.217:8081/H/" + houseId);
             request.ContentType = "application/json";
@@ -74,9 +119,9 @@ namespace HomeAutomationServer.Services
             return null;
         }*/
 
-        public JObject DeleteHouse(string houseid)
-        {
-            /*WebRequest request = WebRequest.Create("http://54.152.190.217:8081/H/" + houseid);
+		public JObject DeleteHouse(string houseid)
+		{
+			/*WebRequest request = WebRequest.Create("http://54.152.190.217:8081/H/" + houseid);
            request.Method = "DELETE";
 
            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
@@ -93,10 +138,10 @@ namespace HomeAutomationServer.Services
                return JObject.Parse(houseString);
            }*/
 
-            return null;
-        }
+			return null;
+		}
 
-        /*public Exception UpdateHouse(int id, string name, int userId)
+		/*public Exception UpdateHouse(int id, string name, int userId)
         {
             //House house = new House();
             //house = getHouse(id);                             // Persistent storage getHouse() method
@@ -128,5 +173,5 @@ namespace HomeAutomationServer.Services
             return null;
 
         }*/
-    }
+	}
 }
