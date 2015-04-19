60c6fa9bccfbb3d854eaaf72d99e146ebc82d06d
enghokie <enghokie@users.noreply.github.com>
2015-04-19 15:49:10 -0400

    Added brighten method

--- HomeAutomationServer/Services/UserRepository.cs
+++ HomeAutomationServer/Services/UserRepository.cs
@@ -1,4 +1,4 @@
-﻿using HomeAutomationServer.Services;
+using HomeAutomationServer.Services;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using System;
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 using System.Net;
 using System.Web;
+using System.Text;
 
 // This class tells the controller how to process the HTTP commands
 
@@ -14,60 +15,128 @@ namespace HomeAutomationServer.Services
 {
     public class UserRepository
     {
+        private string path = @"C:\ServerAPILogFile\logfile.txt";
+
         public JObject GetUser(string username)
         {
-            WebRequest request = WebRequest.Create("http://54.152.190.217:8080/UI/" + username);
-            request.Method = "GET";
-
-            using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+            try
             {
-                if (response.StatusCode != HttpStatusCode.OK)
-                    throw new Exception(String.Format(
-                    "Server error (HTTP {0}: {1}).",
-                    response.StatusCode,
-                    response.StatusDescription));
-                var stream = response.GetResponseStream();
-                var reader = new StreamReader(stream);
+                WebRequest request = WebRequest.Create("http://172.31.26.85:8080/UI/" + username);
+                request.Method = "GET";
 
-                string userString = reader.ReadToEnd();
-                return JObject.Parse(userString);
+                try
+                {
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        if (response.StatusCode != HttpStatusCode.OK)
+                            throw new Exception(String.Format(
+                            "Server error (HTTP {0}: {1}).",
+                            response.StatusCode,
+                            response.StatusDescription));
+                        var stream = response.GetResponseStream();
+                        var reader = new StreamReader(stream);
+
+                        string userString = reader.ReadToEnd();
+                        return JObject.Parse(userString);
+                    }
+                }
+
+                catch (Exception ex)
+                {
+                    if (!File.Exists(path))
+                    {
+                        Directory.CreateDirectory(@"C:\ServerAPILogFile");
+                        using (FileStream fstream = File.Create(path))
+                        {
+                            Byte[] info = new UTF8Encoding(true).GetBytes("Could not Get user information from Storage: " + ex.Message);
+                            fstream.Write(info, 0, info.Length);
+                        }
+                    }
+                    else File.AppendAllText(path, "\nUpdateLocation -- Could not create the URL with the data provided: " + ex.Message);
+
+                    return null;
+                }
+            }
+
+            catch (SystemException ex)
+            {
+                if (!File.Exists(path))
+                {
+                    Directory.CreateDirectory(@"C:\ServerAPILogFile");
+                    using (FileStream fstream = File.Create(path))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes("UserGet -- Could not create URL from data provided: " + ex.Message);
+                        fstream.Write(info, 0, info.Length);
+                    }
+                }
+                else File.AppendAllText(path, "\nUpdateLocation -- Could not create the URL with the data provided: " + ex.Message);
+
+                return null;
             }
         }
 
         public bool SaveUser(string username, JToken model)
         {
-            WebRequest request = WebRequest.Create("http://54.152.190.217:8081/U/" + username);
-            request.ContentType = "application/json";
-            request.Method = "POST";
-
-            using (var streamWriter = new StreamWriter(request.GetRequestStream()))
-            {
-                streamWriter.Write(model.ToString());
-                streamWriter.Close();
-            }
-            // request = WebRequest.Create("http://localhost:8081");
-
             try
             {
-                using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                WebRequest request = WebRequest.Create("http://54.152.190.217:8081/U/" + username);
+                request.ContentType = "application/json";
+                request.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
                 {
-                    if (response.StatusCode != HttpStatusCode.OK)
-                        throw new Exception(String.Format(
-                        "Server error (HTTP {0}: {1}).",
-                        response.StatusCode,
-                        response.StatusDescription));
+                    streamWriter.Write(model.ToString());
+                    streamWriter.Close();
+                }
+                // request = WebRequest.Create("http://localhost:8081");
+
+                try
+                {
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        if (response.StatusCode != HttpStatusCode.OK)
+                            throw new Exception(String.Format(
+                            "Server error (HTTP {0}: {1}).",
+                            response.StatusCode,
+                            response.StatusDescription));
+                    }
+                }
+
+                catch (Exception we)
+                {
+                    if (!File.Exists(path))
+                    {
+                        Directory.CreateDirectory(@"C:\ServerAPILogFile");
+                        using (FileStream fstream = File.Create(path))
+                        {
+                            Byte[] info = new UTF8Encoding(true).GetBytes("Could not post user information to the Storage: " + we.Message);
+                            fstream.Write(info, 0, info.Length);
+                        }
+                    }
+                    else File.AppendAllText(path, "\nUpdateLocation -- Could not create the URL with the data provided: " + we.Message);
+
+                    return false;
                 }
             }
 
-            catch (WebException we)
+            catch (SystemException ex)
             {
-                // always catches this exception even when the Jtoken is sent properly. 
-                // Gets an error saying Connection was closed.
+                if (!File.Exists(path))
+                {
+                    Directory.CreateDirectory(@"C:\ServerAPILogFile");
+                    using (FileStream fstream = File.Create(path))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes("UserPost -- Could not create URL from data provided: " + ex.Message);
+                        fstream.Write(info, 0, info.Length);
+                    }
+                }
+                else File.AppendAllText(path, "\nUpdateLocation -- Could not create the URL with the data provided: " + ex.Message);
+
+
+                return false;
             }
 
-            //return null;
             return true;
-            //stubbed, will send an updated positio*/
         }
 
         public JObject DeleteUser(string username)
@@ -120,15 +189,99 @@ namespace HomeAutomationServer.Services
                 }
                 catch (WebException we)
                 {
-                    File.AppendAllText("HomeAutomationServer/logfile.txt", "Could not Post data to Decision System: " + we.Message);
-                    return false;
-                }
+                    if (!File.Exists(path))
+                    {
+                        Directory.CreateDirectory(@"C:\ServerAPILogFile");
+                        using (FileStream fstream = File.Create(path))
+                        {
+                            Byte[] info = new UTF8Encoding(true).GetBytes("UpdateLocation -- Could not post location change to Decision System: " + we.Message);
+                            fstream.Write(info, 0, info.Length);
+                        }
+                    }
+                    else File.AppendAllText(path, "\nUpdateLocation -- Could not create the URL with the data provided: " + we.Message);
 
+                    return false;
+
+                }
             }
 
             catch (SystemException ex)
             {
-                File.AppendAllText("HomeAutomationServer/logfile.txt", ex.Message);
+                if (!File.Exists(path))
+                {
+                    Directory.CreateDirectory(@"C:\ServerAPILogFile");
+                    using (FileStream fstream = File.Create(path))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes("UpdateLocation -- Could not create the URL with the data provided: " + ex.Message);
+                        fstream.Write(info, 0, info.Length);
+                    }
+                }
+                else File.AppendAllText(path, "\nUpdateLocation -- Could not create the URL with the data provided: " + ex.Message);
+
+                return false;
+            }
+
+            return true;
+        }
+
+        //Sends an updated position to the decison system and needs the nearest object to be brightened that can be brightened
+        public bool Brighten(JObject model)
+        {
+            try
+            {
+                WebRequest request = WebRequest.Create("http://54.152.190.217:8085/LocationChange");
+                request.ContentType = "application/json";
+                request.Method = "POST";
+
+                using (var streamWriter = new StreamWriter(request.GetRequestStream()))
+                {
+                    streamWriter.Write(model.ToString());
+                    streamWriter.Flush();
+                    streamWriter.Close();
+                }
+
+                try
+                {
+                    using (HttpWebResponse response = request.GetResponse() as HttpWebResponse)
+                    {
+                        if (response.StatusCode != HttpStatusCode.OK)
+                            throw new Exception(String.Format(
+                            "Server error (HTTP {0}: {1}).",
+                            response.StatusCode,
+                            response.StatusDescription));
+                    }
+                }
+                catch (Exception we)
+                {
+                    if (!File.Exists(path))
+                    {
+                        Directory.CreateDirectory(@"C:\ServerAPILogFile");
+                        using (FileStream fstream = File.Create(path))
+                        {
+                            Byte[] info = new UTF8Encoding(true).GetBytes("Brigthen -- Could not post information to Decision System: " + we.Message);
+                            fstream.Write(info, 0, info.Length);
+                        }
+                    }
+                    else File.AppendAllText(path, "\nBrighten -- Could not post information to Decision System: " + we.Message);
+
+                    return false;
+
+                }
+            }
+
+            catch (SystemException ex)
+            {
+                if (!File.Exists(path))
+                {
+                    Directory.CreateDirectory(@"C:\ServerAPILogFile");
+                    using (FileStream fstream = File.Create(path))
+                    {
+                        Byte[] info = new UTF8Encoding(true).GetBytes("Brigthen -- Could not create the URL with the data provided: " + ex.Message);
+                        fstream.Write(info, 0, info.Length);
+                    }
+                }
+                else File.AppendAllText(path, "\nBrighten -- Could not create the URL with the data provided: " + ex.Message);
+
                 return false;
             }
 
