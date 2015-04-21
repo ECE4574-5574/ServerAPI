4060789d5d9b96efe6531bdbe30b6926cc5068d1
enghokie <enghokie@users.noreply.github.com>
2015-04-20 22:41:26 -0400

    Create LogFile.cs

---
+++ HomeAutomationServer/Models/LogFile.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HomeAutomationServer.Models
+{
+    static public class LogFile
+    {
+        static private List<string> logfile = new List<string>();
+
+        static public bool AddLog(string log)
+        {
+            logfile.Add(log);
+            if (logfile.Last() == log) return true;
+            else return false;
+        }
+
+        static public string GetLog()
+        {
+            return logfile.ToString();
+        }
+
+        static public void DeleteLog()
+        {
+            logfile.Clear();
+        }
+
+        static public int GetCount()
+        {
+            return logfile.Count;
+        }
+    }
+}
